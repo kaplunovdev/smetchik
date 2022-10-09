ff754ff1f07b794ad91be12e2f4f004c83cfbc6d
Alex <kaplunovdev@gmail.com>
2022-10-09 11:49:10 +0300

    add button 'New material'

--- src/components/Material/Material.jsx
+++ src/components/Material/Material.jsx
@@ -1,65 +1,68 @@
 import React from "react";
-import style from './Material.module.css'
-import {TextField} from "@mui/material";
-import {useDispatch, useSelector} from "react-redux";
+import style from "./Material.module.css";
+import { TextField } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
 import {
-    actionPriceCement,
-    actionPricePesok,
-    actionPricePlitka,
-    actionPricePorebrik,
-    actionPriceSheben
+  actionPriceCement,
+  actionPricePesok,
+  actionPricePlitka,
+  actionPricePorebrik,
+  actionPriceSheben,
 } from "../../redux/reducerMaterials";
+import AddIconMaterials from "@mui/icons-material/AddCircleOutline";
 
 export const Material = () => {
-    const plitkaPrice = useSelector(state => state.materialPage.plitkaPrice)
-    const pricePesok = useSelector(state => state.materialPage.pricePesok)
-    const priceSheben = useSelector(state => state.materialPage.priceSheben)
-    const porebrikPrice = useSelector(state => state.materialPage.porebrikPrice)
-    const priceCement = useSelector(state => state.materialPage.priceCement)
+  const plitkaPrice = useSelector((state) => state.materialPage.plitkaPrice);
+  const pricePesok = useSelector((state) => state.materialPage.pricePesok);
+  const priceSheben = useSelector((state) => state.materialPage.priceSheben);
+  const porebrikPrice = useSelector(
+    (state) => state.materialPage.porebrikPrice
+  );
+  const priceCement = useSelector((state) => state.materialPage.priceCement);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    return (
-        <div>
-            <div className={style.formInputs}>
-                <div className={style.formInputsItem}>
-                    <TextField
-                        onChange={e => dispatch(actionPricePlitka(e.target.value))}
-                       value={plitkaPrice}
-                        label="Цена плитки"
-                    />
-                </div>
-                <div className={style.formInputsItem}>
-                    <TextField
-                        onChange={e => dispatch(actionPricePesok(e.target.value))}
-                        value={pricePesok}
-                        label="Цена песка"
-                    />
-                </div>
-                <div className={style.formInputsItem}>
-                    <TextField
-                        onChange={e => dispatch(actionPriceSheben(e.target.value))}
-                        value={priceSheben}
-                        label="Цена щебня"
-                    />
-                </div>
-                <div className={style.formInputsItem}>
-                    <TextField
-                        onChange={e => dispatch(actionPricePorebrik(e.target.value))}
-                        value={porebrikPrice}
-                        label="Цена поребрика "
-                    />
-                </div>
-
-                <div className={style.formInputsItem}>
-                    <TextField
-                        onChange={e => dispatch(actionPriceCement(e.target.value))}
-                        value={priceCement}
-                        label="Цена цемента"
-                    />
-                </div>
-            </div>
-
+  return (
+    <div>
+      <div className={style.formInputs}>
+        <div className={style.formInputsItem}>
+          <TextField
+            onChange={(e) => dispatch(actionPricePlitka(e.target.value))}
+            value={plitkaPrice}
+            label="Цена плитки"
+          />
         </div>
-    );
+        <div className={style.formInputsItem}>
+          <TextField
+            onChange={(e) => dispatch(actionPricePesok(e.target.value))}
+            value={pricePesok}
+            label="Цена песка"
+          />
+        </div>
+        <div className={style.formInputsItem}>
+          <TextField
+            onChange={(e) => dispatch(actionPriceSheben(e.target.value))}
+            value={priceSheben}
+            label="Цена щебня"
+          />
+        </div>
+        <div className={style.formInputsItem}>
+          <TextField
+            onChange={(e) => dispatch(actionPricePorebrik(e.target.value))}
+            value={porebrikPrice}
+            label="Цена поребрика "
+          />
+        </div>
+
+        <div className={style.formInputsItem}>
+          <TextField
+            onChange={(e) => dispatch(actionPriceCement(e.target.value))}
+            value={priceCement}
+            label="Цена цемента"
+          />
+        </div>
+      </div>
+      <AddIconMaterials className={style.AddIconMaterials} />
+    </div>
+  );
 };
